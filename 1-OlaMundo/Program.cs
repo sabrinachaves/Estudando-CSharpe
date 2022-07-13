bd130dd6e32aa7e73dff199c8d344708eff48561
sabrinachs <sabrinach08@yahoo.com.br>
2022-07-13 08:59:04 -0300

    Adicionar arquivos de projeto.

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
+
---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int idade;
+        idade = 26;
+
+        idade = 5 * 4 + 3;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
