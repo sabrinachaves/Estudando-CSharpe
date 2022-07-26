0951fa574424c24d5ab192870116c295580b749e
sabrinachs <sabrinach08@yahoo.com.br>
2022-07-26 08:00:35 -0300

    Ponto Flutuante

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante")
+        double salario;
+        salario = 3000.13;
+
+
+        Console.WriteLine("Meu salário é: " + salario);
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ 4-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        double divisaoInteira = 7/5;
+
+        double divisao = 7.0 / 5; //Para aparecer a parte decimal na resposta, um dos números precisa ter casa decimal, mesmo que .0
+
+        Console.WriteLine("O resultado da primeira divisão é: " + divisaoInteira);
+        Console.WriteLine("O resultado da segunda divisão é: " + divisao);
+    }
+}
