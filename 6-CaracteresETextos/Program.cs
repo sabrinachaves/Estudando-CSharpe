97784463b7cb327fa9228cd50003744c4b18e0d5
sabrinachs <sabrinach08@yahoo.com.br>
2022-07-26 08:24:39 -0300

    Char

---
+++ 6-CaracteresETextos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Projeto 5 - Caracteres e Textos\n");
+
+        char letra = 'a';
+        Console.WriteLine(letra + "\n");
+
+        letra = (char)65;
+        Console.WriteLine(letra + "\n");
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra + "\n");
+
+        letra = (char)72;
+        Console.WriteLine(letra + "\n");
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+    
+}
