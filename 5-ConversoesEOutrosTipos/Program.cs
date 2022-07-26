89a5b462c78bd3c41bf1531299a389e95b1a0ac9
sabrinachs <sabrinach08@yahoo.com.br>
2022-07-26 08:15:45 -0300

    Conversões de Tipos

---
+++ 5-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        double salario = 3000.15;
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+
+        long x = 2000000000000000000;
+        Console.WriteLine(x);
+
+        short y;
+        y = 15000;
+        Console.WriteLine(y);
+
+
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle enter para sair...");
+        Console.ReadLine();
+    }
+}
